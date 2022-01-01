de030f2e151c9874058cb024baffa17ae63ecf7e
Rasyid Karomi <waverate@gmail.com>
2022-01-01 21:20:54 +0700

    commit

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.7.3",
+  networks: {
+    hardhat: {
+      chainId: 99999,
+      gas: 12000000,
+      blockGasLimit: 0x1fffffffffffff,
+      allowUnlimitedContractSize: true,
+      timeout: 1800000
+    },
+  }
+};
